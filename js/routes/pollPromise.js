1933a61c660a21bd2ed910c0a19d38e5274e3b3c
Ryan Reich <ryan.reich@gmail.com>
2016-02-23 01:23:48 -0800

    Fix polling b/c of bluebird-3.0 cancellation semantics

--- js/routes/pollPromise.js
+++ js/routes/pollPromise.js
@@ -8,12 +8,21 @@ var defaults = {}
 module.exports.defaults = defaults;
 
 function pollPromise(promiseFn) {
+    var done = false; 
     function doPoll() {
+        if (done) {
+            return;
+        }
         return promiseFn().
             catch(errors.matchTag("NotDone"), function() {
                 return Promise.resolve().delay(defaults.pollEveryMS).then(doPoll);
             });
     }
     
-    return doPoll().timeout(defaults.pollTimeoutMS);
+    return doPoll().
+        timeout(defaults.pollTimeoutMS).
+        catch(function(e) {
+            done = true;
+            throw e;
+        })
 }
